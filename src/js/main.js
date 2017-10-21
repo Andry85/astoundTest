ed7843c46f36c2ba9e4f3ea249c1d70f569e5284
HOME\Andry <shapovala@ukr.net>
2017-10-21 19:17:44 +0300

    fixing css

--- src/js/main.js
+++ src/js/main.js
@@ -1,12 +1,46 @@
 $(function() {
+    "use strict";
 
 	/*********************************************************/
-						/* START   */
+						/* start move elemets   */
 	/********************************************************/
+        moveElements();
+        // run test on resize of the window
+        $(window).resize(moveElements);
+        function moveElements(){
+            if ($(window).width() >= 768 && $(window).width() <= 991) {
+                $(".socialList").prependTo(".itemPicInner");
+            } else if ($(window).width() >= 992) {
+                $(".socialList").appendTo(".socialPopapWrap__inner");
+                $(".title").prependTo(".itemInfo");
+            }
+            else {
+                $(".socialList").insertAfter(".userReaction");
+                $(".title").prependTo(".itemPic");
+            }
+
+        }
 
 	/*********************************************************/
-						/* END  */
+						/* end move elemets  */
 	/********************************************************/
+
+
+    /*********************************************************/
+    /* start show popap   */
+    /********************************************************/
+        $(".socialPopapWrap__title").click(function(){
+            $(this).next(".socialPopapWrap__inner").fadeIn('slow');
+        });
+        $(".socialPopapWrap__cloze").click(function(){
+            $(this).parent(".socialPopapWrap__inner").fadeOut('slow');
+        });
+    /*********************************************************/
+    /* end show popap  */
+    /********************************************************/
+
+
+
 
 
 
