f28481dc5746bcb967d6302df0bab002b6364f34
HOME\Andry <shapovala@ukr.net>
2017-10-21 23:55:01 +0300

    slider work

---
+++ src/js/jquery.carusel.js
@@ -0,0 +1,67 @@
+﻿(function($){
+    jQuery.fn.carusel = function(options){
+        options = $.extend({
+            count: 1
+        }, options);
+
+        var make = function(){
+            $(this).wrap("<div class='caruselInner'></div>");
+            $(this).children('li').each(function( index ) {
+                $(this).replaceWith( "<div class='caruselInner__item'>" + $( this ).html() + "</div>");
+            });
+            $(this).replaceWith( "<div class='caruselInner__slider'>" + $( this ).html() + "</div>" );
+            $('.caruselWrap').append("<div class='caruselInner__left'></div><div class='caruselInner__right'></div>");
+            var currentwidth = $('.caruselInner__item').width();
+            $('.caruselInner__item').width(currentwidth/options.count);
+
+            var itemSumWidth = 0;
+            $('.caruselInner__item').each(function() {
+                itemSumWidth = itemSumWidth + $(this).width();
+            });
+
+            $('.caruselInner__slider').width(itemSumWidth);
+
+            var sliderWidth = $('.caruselInner__slider').width();
+            var sliderWidthNumber = parseInt(sliderWidth);
+
+
+            $('.caruselInner__left').click(function(e){
+
+                var leftPos = $('.caruselInner__slider').css('left');
+                var leftPosNumber = parseInt(leftPos);
+                var leftPosNumberPlus = Math.abs(leftPosNumber);
+
+                if (sliderWidthNumber - currentwidth <= leftPosNumberPlus) {
+                    e.stopImmediatePropagation();
+                    $(this).css('opacity', 0.5);
+                } else {
+                    $('.caruselInner__slider').animate({
+                        left: (leftPosNumber - currentwidth) + 'px'
+                    },"slow");
+                    $('.caruselInner__right').css('opacity', 1.0);
+                }
+            });
+
+            $('.caruselInner__right').click(function(e){
+
+                var leftPos = $('.caruselInner__slider').css('left');
+                var leftPosNumber = parseInt(leftPos);
+
+                if (leftPosNumber >= 0) {
+                    e.stopImmediatePropagation();
+                    $(this).css('opacity', 0.5);
+                } else {
+                    $('.caruselInner__slider').animate({
+                        left: (leftPosNumber + currentwidth) + 'px'
+                    },"slow");
+                    $('.caruselInner__left').css('opacity', 1.0);
+                }
+            });
+
+
+
+        };
+
+        return this.each(make);
+    };
+})(jQuery);
--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
-$(function() {
-    "use strict";
+(function($){
+
 
 	/*********************************************************/
 						/* start move elemets   */
@@ -29,20 +29,37 @@ $(function() {
     /*********************************************************/
     /* start show popap   */
     /********************************************************/
-        $(".socialPopapWrap__title").click(function(){
-            $(this).next(".socialPopapWrap__inner").fadeIn('slow');
-        });
-        $(".socialPopapWrap__cloze").click(function(){
-            $(this).parent(".socialPopapWrap__inner").fadeOut('slow');
-        });
+    $(".socialPopapWrap__title").click(function(){
+        $(this).next(".socialPopapWrap__inner").fadeIn('slow');
+    });
+    $(".socialPopapWrap__cloze").click(function(){
+        $(this).parent(".socialPopapWrap__inner").fadeOut('slow');
+    });
     /*********************************************************/
     /* end show popap  */
+    /********************************************************/
+
+    /*********************************************************/
+    /* start slider   */
+    /********************************************************/
+        caruselRun();
+        // run test on resize of the window
+        $(window).resize(caruselRun);
+        function caruselRun(){
+            if ($(window).width() >= 768) {
+                location.reload(true);
+
+            } else {
+                $('#slider').carusel();
+            }
+
+
+        }
+    /*********************************************************/
+    /* end slider  */
     /********************************************************/
 
 
 
 
-
-
-
-});
+})(jQuery);
